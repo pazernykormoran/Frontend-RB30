025f1d52e117b72af30b1d2f23a3f95a38686c94
marek-wojtowicz-accenture <marek.wojtowicz@accenture.com>
2021-07-29 22:45:17 +0200

    Simple fetching app using API

---
+++ fetch-app/fetch-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>A fucking simple app to fetch API</h1>
+    <EmployeeList />
+  </div>
+</template>
+
+<script>
+import EmployeeList from './components/EmployeeList.vue'
+export default {
+  name: 'App',
+  components: {
+    EmployeeList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
